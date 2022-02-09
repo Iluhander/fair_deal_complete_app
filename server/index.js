1d4e96d88584ea352c4d5dc73f4b1d05fac1408b
Iluhander <iisergeev_2@edu.hse.ru>
2022-02-09 04:34:35 +0300

    Finished minimal client-server interaction

--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
-console.log('XYEK')
-
 require('dotenv').config()
 const express = require('express')
+const path = require("path");
 
 const PORT = process.env.PORT || 2228
 
 const app = express()
+
+app.use("/static", express.static(path.join(__dirname, '/../client/build/')));
+
+app.get('/main', function(req, res) {
+    res.sendFile(path.join(__dirname,'/../client/build/index.html'))
+});
+
+app.get('/manifest.json', function(req, res) {
+    res.sendFile(path.join(__dirname,'/../client/build/manifest.json'))
+});
+
+app.get('/static/js/main.f5dc9d9b.js', function(req, res) {
+    res.sendFile(path.join(__dirname,'/../client/build/static/js/main.f5dc9d9b.js'))
+});
+
+app.get('/static/css/main.f13d7f65.css', function(req, res) {
+    res.sendFile(path.join(__dirname,'/../client/build/static/css/main.f13d7f65.css'))
+});
 
 app.listen(PORT, () => console.log('OK!'))
