d7939fde679222355a8d8ee091e6b64d01bfc356
Iluhander <iisergeev_2@edu.hse.ru>
2022-03-15 13:38:45 +0300

    Changed background (leaves)

--- client/src/App.js
+++ client/src/App.js
@@ -8,26 +8,28 @@ import {useState} from "react";
 
 function App() {
   const [contentClass, setContentClass] = useState('');
+  const [animationIterations, setAnimationIterations] = useState(1);
 
   function changeBlur() {
-    if (contentClass != 'blured') {
+    if (contentClass !== 'blured') {
       setContentClass('blured');
     } else {
       setContentClass('unblured');
     }
+
+    setAnimationIterations(animationIterations + 1);
   }
 
   return (
       <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
-        <Greetings changeVisibility={changeMenuVisibility}/>
-        <Menu changeVisibility={changeMenuVisibility} changeBlur={changeBlur}/>
-        <div className={contentClass}>
-          <TextBlock h1='Сделки с Fair Deal - это безопасно' p='Fair Deal - сервис, помогающий не терять деньги, товар и драгоценное время на мошенников, обеспечивая выполнение всех условий любой онлайн сделки как для заказчика (покупателя), так и для исполнителя (продавца).
-' button='Совершите сделку с нами'/>
-          <InfoSection/>
-          <Basement/>
-        </div>
-
+          <Greetings changeVisibility={changeMenuVisibility}/>
+          <Menu changeVisibility={changeMenuVisibility} changeBlur={changeBlur}/>
+          <div className={contentClass}>
+              <TextBlock h1='Сделки с Fair Deal - это безопасно' p='Fair Deal - сервис, помогающий не терять деньги, товар и драгоценное время на мошенников, обеспечивая выполнение всех условий любой онлайн сделки как для заказчика (покупателя), так и для исполнителя (продавца).
+    ' button='Совершите сделку с нами'/>
+              <InfoSection/>
+              <Basement/>
+          </div>
       </div>
   );
 }
